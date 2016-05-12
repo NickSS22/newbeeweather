b0c55f7391c132077791aa52955c31f2e3eb119c
Sheng Sheng <593605834@qq.com>
2016-05-12 15:23:51 +0800

    新增切换城市和手动更新天气的功能。

--- src/com/example/newbeeweather/activity/ChooseAreaActiviry.java
+++ src/com/example/newbeeweather/activity/ChooseAreaActiviry.java
@@ -11,10 +11,14 @@ import com.example.newbeeweather.util.HttpCallbackListener;
 import com.example.newbeeweather.util.HttpUtil;
 import com.example.newbeeweather.util.Utility;
 
+import android.R.bool;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.app.DownloadManager.Query;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -65,10 +69,26 @@ public class ChooseAreaActiviry extends Activity {
 	 * 选中的级别
 	 */
 	private int currentLevel;
+	
+	/*
+	 * 是否从WeatherActivity中跳转过来
+	 */
+	private boolean isFromWeatherActivity;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		//已经选择了城市且不是从WeatherActivity跳转过来，才会直接跳到WeatherActivity
+		if(prefs.getBoolean("city_selected", false) && !isFromWeatherActivity ){
+			Intent intent = new Intent(this,WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
+		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		listView = (ListView) findViewById(R.id.list_view);
@@ -90,6 +110,13 @@ public class ChooseAreaActiviry extends Activity {
 				} else if (currentLevel == LEVEL_CITY) {
 					selectedCity = cityList.get(position);
 					queryCountries();
+				}else if(currentLevel == LEVEL_COUNTRY){
+					String countryCode = countryList.get(position).getCountryCode();
+					Intent intent = new Intent(ChooseAreaActiviry.this,WeatherActivity.class);
+					intent.putExtra("country_code", countryCode);
+					startActivity(intent);
+					finish();
+					
 				}
 			}
 		});
@@ -248,6 +275,10 @@ public class ChooseAreaActiviry extends Activity {
 		} else if (currentLevel == LEVEL_CITY) {
 			queryProvinces();
 		} else {
+			if(isFromWeatherActivity){
+				Intent intent = new Intent(this, WeatherActivity.class);
+				startActivity(intent);
+			}
 			finish();
 		}
 	}
